90c99e960fa5ea163e84d5e3e11c5956b3b9532c
myrianrochina <90713626+myrianrochina@users.noreply.github.com>
2021-09-28 16:14:25 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -5,7 +5,7 @@ function LED1_On() {
 	console.log("led on");
 	//document.getElementById("sensor").innerHTML="led on";
 	message = new Paho.MQTT.Message("ON");
-    	message.destinationName = "lfrenteriax@hotmail.com/test1";
+    	message.destinationName = "mayra.machado@unach.edu.ec/T1";
     	client.send(message);
   
 }
@@ -13,7 +13,7 @@ function LED1_Off(){
 	//alert("led off");
 	console.log("led off");
 	message = new Paho.MQTT.Message("OFF");
-    	message.destinationName = "lfrenteriax@hotmail.com/test1";
+    	message.destinationName = "mayra.machado@unach.edu.ec/T1";
     	client.send(message);
 	//document.getElementById("sensor").innerHTML="led off";
 }
@@ -33,8 +33,8 @@ function LED1_Off(){
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "lfrenteriax@hotmail.com",
-    password: "lfrenteriax",
+    userName: "mayra.machado@unach.edu.ec",
+    password: "@mayramachado97",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -47,9 +47,9 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("lfrenteriax@hotmail.com/test");
+    client.subscribe("mayra.machado@unach.edu.ec/T2");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
+    message.destinationName = "mayra.machado@unach.edu.ec/T1";
     client.send(message);
 	
   }
@@ -69,6 +69,8 @@ function LED1_Off(){
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor").innerHTML=message.payloadString;
+    var m=message.payloadString.split("*")
+	  document.getElementById("sensor1").innerHTML=m[0];
+    document.getElementById("sensor2").innerHTML=m[1];
   }
   
---
+++ static/js/prueba.js
@@ -0,0 +1,14 @@
+alert ('Hola desde Prueba');
+document.getElementById('sensor').innerHTML=' HOLA AMIGOS '
+function LED1_On(){
+console.log ('Se presiono el botón 1: ')
+document.getElementById('sensor').innerHTML=' LED ENCENDIDO'
+	;}
+function LED1_Off(){
+console.log ('Se presiono el botón 2: ')
+document.getElementById('sensor').innerHTML=' LED APAGADO'
+	;}
+	function VOLVER(){
+console.log ('Se presiono el boton volver: ')
+document.getElementById('sensor').innerHTML=' HOLA AMIGOS'
+	;}
